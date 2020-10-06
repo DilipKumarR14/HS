25e76f6a6caed1558e2865723c089448a7ed0e0c
Dilip <dilipkumar14inc@gmail.com>
2020-10-06 14:44:41 +0530

    modified to get all and specify id aliens

--- JavaProgram/SpringBoot/springbootjpa/src/main/java/com/example/demo/controller/AlienController.java
+++ JavaProgram/SpringBoot/springbootjpa/src/main/java/com/example/demo/controller/AlienController.java
@@ -2,8 +2,10 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.dao.AlienRepo;
@@ -21,12 +23,22 @@ public class AlienController {
 		return "home.jsp";
 	}
 	
+	/**
+	 * add the alien to db
+	 * @param alien
+	 * @return
+	 */
 	@RequestMapping("/addAlien")
 	public String addAlien(Alien alien) {
 		repo.save(alien);
 		return "home.jsp";
 	}
 	
+	/**
+	 * to get the alien
+	 * @param aid
+	 * @return
+	 */
 	@RequestMapping("/getAlien")
 	public ModelAndView getAlien(@RequestParam int aid) {
 		
@@ -46,6 +58,28 @@ public class AlienController {
 		
 		
 		return mv;
+	}
+	
+	@RequestMapping("/aliens")
+	/**
+	 * to return in REST API format(JSON)
+	 * @return
+	 */
+	@ResponseBody
+	/**
+	 * we're not returning view name, just returning the data
+	 * @return
+	 */
+	public String getAliens() {
+		
+		return repo.findAll().toString();
 		
 	}
+	
+	@RequestMapping("/alien/{aid}")
+	@ResponseBody
+	public String getAlienByIDs(@PathVariable("aid") int aid) {
+		return repo.findById(aid).toString();
+	}
+	
 }
